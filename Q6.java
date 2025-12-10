e816b419828dad91524d576e9d28c3f863de3e42
zerng <zerngx@gmail.com>
2025-12-10 15:49:45 +0800

    Initial commit

---
+++ Q6.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static boolean isMirror(int[] a, int[] b) {
+
+        // Convert arrays A and array B to strings
+        String aStr = "";
+        String bStr = "";
+
+        for (int i = 0; i < a.length;i++) {
+            aStr += a[i];
+            bStr += b[i];
+        }
+        
+        // Reverse string aStr and compare with bStr
+        // Introducing StringBuilder for reversing the string
+        // since String is immutable in Java
+        // so we cannot reverse it directly
+        // StringBuilder provides a mutable sequence of characters
+        // and has a reverse() method to reverse the sequence
+        // Finally convert it back to String using toString() method
+
+        String aRev = new StringBuilder(aStr).reverse().toString();
+        return aRev.equals(bStr);
+    }
+
+    public static void main(String[]args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Array A: ");
+
+        String lineA = sc.nextLine();
+        String[] partsA = lineA.split(",");
+
+        int[] a = new int[partsA.length];
+        for (int j = 0; j < partsA.length; j++) {
+            a[j] = Integer.parseInt(partsA[j].trim());
+        }
+
+        System.out.print("Array B: ");
+        String lineB = sc.nextLine();
+        String[] partsB = lineB.split(",");
+        int[] b = new int[partsB.length];
+        for (int j = 0; j < partsB.length; j++) {
+            b[j] = Integer.parseInt(partsB[j].trim());
+        }
+
+        System.out.println("Mirror Pattern: " + isMirror(a,b));
+    }
+}
